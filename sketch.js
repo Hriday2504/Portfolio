f708a84a9d0900aea08d4c470a4f1332c02a5d22
Hriday <hridayagrawal2007@gmail.com>
2025-10-22 08:49:11 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,48 +1,45 @@
-let t
-let b 
-let col
-let x = 0, y = 0, xV = 3, yV = 5
+let xLoc = [], yLoc = [];
+let numSegments = 50;
+let diameter = 100;
 
+let y
 
 function setup() {
- createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
   
- t=select('#title')
-
-  
- b = select('#blurb')
-  
-
+  for (let i = 0; i<numSegments; i++){
+    xLoc[i] = 0;
+    yLoc[i] = 0;
+    
+  }
+  fill(0);
+  stroke (255);
 }
 
+
 function draw() {
- background(100);
-ellipse (100,100,50)
-   //check
+  background(0);
+  xLoc[numSegments-1] = random(width);
+  yLoc[numSegments-1] = random(height);
   
- t.position(x,y)
-  
- b.style('color', col)
-
-  if(x>windowWidth){
-    xV = -xV
+  for (let i=0; i<numSegments; i++){
+    xLoc[i] = xLoc[i+1];
+    yLoc[i] = yLoc[i+1];
+    
+    let d= map (i,0,numSegments,0,PI)
+    
+    d = sin(d);
+    
+    let r = d*255
+    let g = random(0,255)
+    let b = 255- d*255
+    
+    stroke (r,g,b)
+    ellipse(xLoc[i],yLoc[i], d*diameter)
   }
-   if(y>windowHeight){
-    yV = -yV
-  }
-   if(x<0){
-    xV = -xV
-  }
-   if(y<0){
-    yV = -yV
-  }
-
-  
-  x+=xV
-  y+=yV
 }
 
 function mousePressed(){
-  col =   color(random(255), random(255),random(255))
+  print (xLoc)
   
 }
